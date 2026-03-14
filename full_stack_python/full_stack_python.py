c27f15f24ee8c48ece5127790828e6b218faa803
Donato Lucesoli <donatolucesoli@gmail.com>
2026-03-14 12:50:35 -0300

    on_click y relación con métodos del estado

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -7,6 +7,13 @@ from rxconfig import config
 
 class State(rx.State):
     """The app state."""
+    label = "Label original!"
+
+    def cambiar_label(self):
+        if self.label == "Label original!":
+            self.label = "Label modificado!"
+        else:
+            self.label = "Label original!"
 
 
 def index() -> rx.Component:
@@ -14,12 +21,13 @@ def index() -> rx.Component:
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading(State.label, " Welcome to Reflex!", size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
+            rx.button("Haz algo!", on_click=State.cambiar_label),
             rx.link(
                 rx.button("Check out our docs!"),
                 href="https://reflex.dev/docs/getting-started/introduction/",
